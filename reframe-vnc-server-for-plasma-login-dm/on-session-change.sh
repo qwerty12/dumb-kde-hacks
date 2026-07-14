047167434945bfe00c9adc43c7e7e93b11f20677
Faheem <qwerty12@users.noreply.github.com>
2026-07-14 22:36:29 +0100

    Update on-session-change.sh

--- reframe-vnc-server-for-plasma-login-dm/on-session-change.sh
+++ reframe-vnc-server-for-plasma-login-dm/on-session-change.sh
@@ -2,16 +2,17 @@
 
 case "$PAM_TYPE" in
   open_session)
-    systemctl stop reframe-server@
+    systemd-run --collect --no-block --quiet /usr/bin/systemctl stop reframe-server@
     ;;
   close_session)
-    closing="$XDG_SESSION_ID"
-    still_active=0
-    for s in $(loginctl list-sessions --no-legend | awk '{print $1}'); do
-      [ "$s" = "$closing" ] && continue # ignore the session that's ending
-      class=$(loginctl show-session "$s" -p Class --value 2>/dev/null)
-      [ "$class" = "user" ] && { still_active=1; break; }
-    done
-    [ "$still_active" -eq 0 ] && systemctl start reframe-server@
+    systemd-run --collect --no-block --quiet -E XDG_SESSION_ID="$XDG_SESSION_ID" /bin/sh -c '
+      still_active=0
+      for s in $(loginctl list-sessions --no-legend | awk "{print \$1}"); do
+        [ "$s" = "$XDG_SESSION_ID" ] && continue
+        class=$(loginctl show-session "$s" -p Class --value 2>/dev/null)
+        [ "$class" = "user" ] && { still_active=1; break; }
+      done
+      [ "$still_active" -eq 0 ] && systemctl start reframe-server@
+    '
     ;;
 esac
